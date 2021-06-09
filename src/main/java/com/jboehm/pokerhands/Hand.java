2d4e18c6e3634fd9bba3cb3f5d5638ad965337f0
Jérémy Boehm <jeremyboehm77@gmail.com>
2021-06-09 16:19:42 +0200

    Refactor Hand initialization

--- src/main/java/com/jboehm/pokerhands/Hand.java
+++ src/main/java/com/jboehm/pokerhands/Hand.java
@@ -12,27 +12,17 @@ import static java.util.stream.Collectors.toList;
 
 public class Hand {
 
-    private final Card card1;
-    private final Card card2;
-    private final Card card3;
-    private final Card card4;
-    private final Card card5;
+    private final List<Card> cards;
 
     public Hand(Card card1, Card card2, Card card3, Card card4, Card card5) {
-        this.card1 = card1;
-        this.card2 = card2;
-        this.card3 = card3;
-        this.card4 = card4;
-        this.card5 = card5;
+        this.cards = asList(card1, card2, card3, card4, card5);
     }
 
     public Card highestCard() {
-        List<Card> cards = cardsToList(card1, card2, card3, card4, card5);
         return cards.stream().max(Card::compareTo).get();
     }
 
     public boolean hasTwoCardsWithTheSameValue() {
-        List<Card> cards = cardsToList(card1, card2, card3, card4, card5);
 
         return IntStream.range(0, cards.size())
                 .anyMatch(i -> IntStream.range(i + 1, cards.size())
@@ -43,7 +33,6 @@ public class Hand {
     }
 
     public List<Card> selectTwoCardsWithTheSameValue() {
-        List<Card> cards = cardsToList(card1, card2, card3, card4, card5);
         for (int i = 0; i < cards.size(); i++) {
             for (int j = i+1; j < cards.size(); j++) {
                 if (cards.get(i).getCardValue().getAssociatedValue().equals(cards.get(j).getCardValue().getAssociatedValue())) {
@@ -55,14 +44,9 @@ public class Hand {
     }
 
     public List<Card> allCardsExcept(Card... cardsToExclude) {
-        List<Card> cards = cardsToList(card1, card2, card3, card4, card5);
         return cards.stream()
                 .filter(card -> !asList(cardsToExclude).contains(card))
                 .collect(toList());
-    }
-
-    private List<Card> cardsToList(Card card1, Card card2, Card card3, Card card4, Card card5) {
-        return Arrays.asList(card1, card2, card3, card4, card5);
     }
 
 }
